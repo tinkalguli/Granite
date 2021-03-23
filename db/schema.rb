846715c4c5c1a1ac150bd24bdd91751bb07d6fb1
tinkalguli <tinkalmunu@gmail.com>
2021-03-23 14:39:35 +0530

    Added constraint to make title not nullable

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_082437) do
+ActiveRecord::Schema.define(version: 2021_03_23_090635) do
 
   create_table "tasks", force: :cascade do |t|
-    t.text "title"
+    t.text "title", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
