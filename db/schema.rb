bc3a8ffc14c3e85cc6dd7a9134d5bec87e8f8fce
tinkalguli <tinkalmunu@gmail.com>
2021-03-25 00:48:55 +0530

    Added has_secure_password and validations to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_24_113317) do
+ActiveRecord::Schema.define(version: 2021_03_24_190122) do
 
   create_table "tasks", force: :cascade do |t|
     t.text "title", null: false
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2021_03_24_113317) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "tasks", "users", on_delete: :cascade
